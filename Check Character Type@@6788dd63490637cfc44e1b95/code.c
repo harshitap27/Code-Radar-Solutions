09296bb93394590c8dddf5e520c2b48325fa2a98
Harshita <harshitapruthijmd@gmail.com>
2025-03-20 20:40:34 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -14,6 +14,6 @@ int main(){
         printf("Digit");
             }
     else{
-                printf("Special Character");
-            }
+        printf("Special Character");
+                   }
 }
